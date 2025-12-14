5ae87a1168d2662d27103a44e43906b038aed6cc
xiahe173 <qh22189009@gmail.com>
2025-12-14 18:31:42 +0800

    Update flag aaf file

--- main/mmap_generate_moji_emoji.h
+++ main/mmap_generate_moji_emoji.h
@@ -14,7 +14,7 @@
 #include "esp_mmap_assets.h"
 
 #define MMAP_MOJI_EMOJI_FILES           22
-#define MMAP_MOJI_EMOJI_CHECKSUM        0xB0BF
+#define MMAP_MOJI_EMOJI_CHECKSUM        0x8E4C
 
 enum MMAP_MOJI_EMOJI_LISTS {
     MMAP_MOJI_EMOJI_ANGRY_AAF = 0,        /*!< angry.aaf */
